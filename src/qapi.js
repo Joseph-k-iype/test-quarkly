7fc222c886c5e1f05c34ece5de42bcaf55e7b008
Joseph K Iype <40719207+Joseph-k-iype@users.noreply.github.com>
2020-12-12 15:15:38 +0530

    Initial commit

---
+++ src/qapi.js
@@ -0,0 +1,25 @@
+export default {
+	"pages": {
+		"root": {
+			"id": "root",
+			"pageUrl": "root",
+			"name": "root",
+			"children": [
+				"5fd490f700ea36001e3b9df7",
+				"5fd490f700ea36001e3b9df9"
+			]
+		},
+		"5fd490f700ea36001e3b9df7": {
+			"id": "5fd490f700ea36001e3b9df7",
+			"name": "404",
+			"pageUrl": "404"
+		},
+		"5fd490f700ea36001e3b9df9": {
+			"id": "5fd490f700ea36001e3b9df9",
+			"name": "index",
+			"pageUrl": "index"
+		}
+	},
+	"mode": "production",
+	"projectType": "create-react-app"
+}
